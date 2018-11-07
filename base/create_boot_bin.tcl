6ae5b78c67d6e7470025dc6574f434867b0fddb9
t_kuha <imagingtechnerd@gmail.com>
2018-11-07 09:59:29 +0900

    Change baremetal app name to "led_blink"

--- base/create_boot_bin.tcl
+++ base/create_boot_bin.tcl
@@ -28,7 +28,7 @@ createapp -name fsbl -app {Zynq MP FSBL} -hwproject bsp -proc psu_cortexa53_0 -o
 createapp -name pmufw -app {ZynqMP PMU Firmware} -hwproject bsp -proc psu_pmu_0 -os standalone
 
 # Create application
-createapp -name hello_world -app {Hello World} -hwproject bsp -proc psu_cortexa53_0 -os standalone
+createapp -name led_blink -app {Hello World} -hwproject bsp -proc psu_cortexa53_0 -os standalone
 
 # Use UART1 as stdin & stdout 
 configbsp -bsp fsbl_bsp stdin  "psu_uart_1"
@@ -41,16 +41,19 @@ configbsp -bsp pmufw_bsp stdout "psu_uart_1"
 updatemss -mss ${XSDK_DIR}/pmufw_bsp/system.mss
 regenbsp -bsp pmufw_bsp
 
-configbsp -bsp hello_world_bsp stdin  "psu_uart_1"
-configbsp -bsp hello_world_bsp stdout "psu_uart_1"
-updatemss -mss ${XSDK_DIR}/hello_world_bsp/system.mss
-regenbsp -bsp hello_world_bsp
+configbsp -bsp led_blink_bsp stdin  "psu_uart_1"
+configbsp -bsp led_blink_bsp stdout "psu_uart_1"
+updatemss -mss ${XSDK_DIR}/led_blink_bsp/system.mss
+regenbsp -bsp led_blink_bsp
 
+# Replace helloworld.c with led_blink.c
+exec rm ${XSDK_DIR}/led_blink/${SRC_DIR}/helloworld.c
+exec cp ${SRC_DIR}/led_blink.c ${XSDK_DIR}/led_blink/${SRC_DIR}/led_blink.c
 
 # Build in release mode
 configapp -app fsbl build-config release
 configapp -app pmufw build-config release
-configapp -app hello_world build-config release
+configapp -app led_blink build-config release
 
 projects -build
 
