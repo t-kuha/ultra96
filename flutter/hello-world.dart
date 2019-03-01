5d5202a95952c688b3eee71800b524624dcc7ed9
t_kuha <imagingtechnerd@gmail.com>
2019-03-01 19:29:05 +0900

    Add dart executable & sample code

---
+++ flutter/hello-world.dart
@@ -0,0 +1,10 @@
+// Define a function.
+printInteger(int aNumber) {
+  print('The number is $aNumber.'); // Print to console.
+}
+
+// This is where the app starts executing.
+main() {
+  var number = 42; // Declare and initialize a variable.
+  printInteger(number); // Call a function.
+}
